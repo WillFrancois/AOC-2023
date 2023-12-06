d01b8108eeca2e5cb98bdec31f319651ab507bb5
WillFrancois <williamjcfrancois@gmail.com>
2023-12-06 08:30:09 -0500

    Modified file to better show relationship between distance and time

--- day6/part1+2/q_formula.py
+++ day6/part1+2/q_formula.py
@@ -5,7 +5,9 @@ def quadratic_formula(b, c):
     x1 = (-b - math.sqrt(b**2 - (4 * c))) / 2
     x2 = (-b + math.sqrt(b**2 - (4 * c))) / 2
 
-    print(x1, x2)
+    return int(x1), int(x2)
 
 
-print(quadratic_formula(71530, 940200))
+x1, x2 = quadratic_formula(71530, 940200)
+
+print(abs(x2 - x1))
