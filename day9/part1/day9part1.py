af79f02388d027e25cdf603e637547d3e6ca3c0c
WillFrancois <williamjcfrancois@gmail.com>
2023-12-10 15:45:14 -0500

    Day 9 Completed

---
+++ day9/part1/day9part1.py
@@ -0,0 +1,25 @@
+file = open("test_input.txt")
+lines = file.readlines()
+lines = list(map(lambda x: x.strip(), lines))
+lines = list(map(lambda x: x.split(" "), lines))
+result = 0
+
+for line in range(len(lines)):
+    lines[line] = list(map(lambda x: int(x), lines[line]))
+
+
+def rec_list_call(input_list: list):
+    if len(list(dict.fromkeys(input_list))) == 1:
+        return input_list[0]
+
+    new_list = []
+    for i in range(1, len(input_list)):
+        new_list.append(input_list[i] - input_list[i-1])
+
+    return input_list[len(input_list) - 1] + rec_list_call(new_list)
+
+
+for line in lines:
+    result += rec_list_call(line)
+
+print(result)
